f261565951928ed6c5c5b10b9f85e429730eeab1
stefftek <spfahler@t-online.de>
2021-03-11 20:31:30 +0100

    Test.png

--- Planet.js
+++ Planet.js
@@ -66,7 +66,7 @@ function percentToHex(percent) {
 }
 
 // Version 4.0
-const pSBC=(p,c0,c1,l)=>{
+function pSBC(p,c0,c1,l) {
     let r,g,b,P,f,t,h,i=parseInt,m=Math.round,a=typeof(c1)=="string";
     if(typeof(p)!="number"||p<-1||p>1||typeof(c0)!="string"||(c0[0]!='r'&&c0[0]!='#')||(c1&&!a))return null;
     if(!this.pSBCr)this.pSBCr=(d)=>{
