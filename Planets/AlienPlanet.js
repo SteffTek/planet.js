e14561cec742170e040f85ee9bc997ff5708acc2
stefftek <spfahler@t-online.de>
2021-03-11 21:16:08 +0100

    Test Planets

---
+++ Planets/AlienPlanet.js
@@ -0,0 +1,38 @@
+let colors = {
+    land_color: "#fde4de",
+    beach_color: "#ed94db",
+    shore_color: "#b9b3ee",
+    ocean_color: "#5588ec",
+    mountain_color: "#f5e7a3",
+    mountain_top_color: "#ffffff",
+    cloud_color: "#ffffff",
+    cloud_opacity: 70,
+    atmosphere_color: "#eae1ee",
+    atmosphere_opacity: 40,
+    add_detail: true,
+}
+let planet_options = {
+    planet_radius: 80,
+    atmosphere_radius: 100,
+    sea_level: 0.42,
+    shore_level: 0.48,
+    beach_level: 0.5,
+    mountain_level: 0.62,
+    mountain_top_level: 0.75,
+    cloud_level: 0.62,
+    cloud_radius: 82,
+}
+
+let generator_options = {
+    octaveCount: 6,
+    amplitude: 2,
+    persistence: 0.5
+}
+
+let cloud_generator = {
+    octaveCount: 6,
+    amplitude: 6,
+    persistence: 0.4
+}
+
+let size = 200;
---
+++ Planets/Moon.js
@@ -0,0 +1,40 @@
+let colors = {
+    land_color: "#585858",
+    beach_color: "#404040",
+    shore_color: "#303030",
+    ocean_color: "#181818",
+    mountain_color: "#606060",
+    mountain_top_color: "#606060",
+    cloud_color: "#ffffff",
+    cloud_opacity: 70,
+    atmosphere_color: "#eae1ee",
+    atmosphere_opacity: 40,
+    add_detail: true,
+}
+let planet_options = {
+    planet_radius: 80,
+    atmosphere_radius: 100,
+    sea_level: 0.42,
+    shore_level: 0.48,
+    beach_level: 0.5,
+    mountain_level: 0.62,
+    mountain_top_level: 0.75,
+    cloud_level: 0.62,
+    cloud_radius: 82,
+}
+
+let generator_options = {
+    octaveCount: 6,
+    amplitude: 3,
+    persistence: 0.5
+}
+
+let cloud_generator = {
+    octaveCount: 6,
+    amplitude: 6,
+    persistence: 0.4
+}
+
+let size = 200;
+
+//AND ATMOSPHERE AND CLOUDS SET TO FALSE
